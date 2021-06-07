b3ff03d58743ec8c0167a5dfee4f5e3ceb689817
joris-verguldezoone <joris.verguldezoone@laplateforme.io>
2021-06-07 14:40:15 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Controller\Controller;
+
+
+session_start();
+// var_dump($_SESSION);
+spl_autoload_register(function ($className) {
+    $className = str_replace('App', 'src', $className);
+    $filePath =  str_replace('\\', '/', $className) . '.php';
+    if (file_exists($filePath)) {
+        require($filePath);
+    }
+});
+
+$app = AppFactory::create();
+
+define('BASE_PATH', rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/'));
+define('HTTP_HOST', $_SERVER["HTTP_HOST"]);
+$app->setBasePath(BASE_PATH);
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', controller::class . ':home');
+
+$app->run();
---
+++ src/controller/Controller.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Controller
+{
+    protected $twig;
+
+    public function home(Request $request, Response $response, $args)
+    {
+        $this->preloadTwig();
+        $response->getBody()->write($this->twig->render('home.twig.html'));
+        return $response;
+    }
+    /**
+     * Charge les fichiers .twig
+     */
+    public function preloadTwig()
+    {
+        $loader = new FilesystemLoader('views');
+        $this->twig = new Environment($loader);
+
+        $this->twig->addGlobal('session', $_SESSION);
+        $this->twig->addGlobal('BASE_PATH', BASE_PATH);
+        $this->twig->addGlobal('HTTP_HOST', HTTP_HOST);
+    }
+
+    /**
+     * Permet de sécuriser les variables passer en paramètre
+     *
+     * @param $var
+     * @return string
+     */
+    public function secure($var): string
+    {
+        return $var = htmlspecialchars(trim($var));
+    }
+
+    /**
+     * Permet de rediriger l'utilisateur dans un chemin donné en paramètre
+     *
+     * @param string $path
+     */
+    public function redirect(string $path)
+    {
+        header('location: ' . $path);
+        exit();
+    }
+}
