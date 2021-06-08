3550146823f8278fe185f2d888e7df1fb202cd9d
huongmay-nguyenphuoc <huong-may.nguyen-phuoc@laplateforme.io>
2021-06-08 15:41:29 +0200

    nav

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,12 @@
-$('#icone').click(function () {
-    $('main').css('transform', 'translateY(0vh)');
-    $('header').css('transform', 'translateY(0vh)');
+$('body').on('click', '#icone', function () {
+    if (!($(this).is('.toggleOn'))) {
+        $(this).addClass('toggleOn')
+        $('main').css('transform', 'translateY(0vh)');
+        $('header').css('transform', 'translateY(0vh)');
+    } else {
+        $(this).removeClass('toggleOn')
+        $('main').css('transform', 'translateY(-100vh)');
+        $('header').css('transform', 'translateY(-100vh)');
+
+    }
 })
